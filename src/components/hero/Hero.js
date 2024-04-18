78cce1f70df66f1d88cf5da2f02ef6dd61a3c161
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-18 16:44:46 +0200

    update

--- src/components/hero/Hero.js
+++ src/components/hero/Hero.js
@@ -2,8 +2,16 @@ import React from 'react';
 import './Hero.css';
 import { Carousel } from 'bootstrap';
 import { Paper } from '@mui/material';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCirclePlay } from '@fortawesome/free-solid-svg-icons';
+import {Link, useNavigate} from "react-router-dom";
+import Button from 'react-bootstrap/Button';
+
 
 const Hero = ({movies}) => {
+
+    const navigate = useNavigate();
+
     return (
         <div className='movie-carousel-container'>
             <Carousel>
