c6aa99dd5d6ca3857315db2d93169cc324935374
J4d4-M4thele <jadamaadimo.mathele@gmail.com>
2024-04-18 17:23:48 +0200

    update

--- src/components/trailer/Trailer.js
+++ src/components/trailer/Trailer.js
@@ -2,7 +2,7 @@ import {useParams} from 'react-router-dom';
 import ReactPlayer from 'react-player';
 import './Trailer.css';
 
-import React from 'react';
+import React from 'react'
 
 const Trailer = () => {
 
